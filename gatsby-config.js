362c7fe69db9abaae4470626635b0d2c65f78847
Nola1222 <noahvanboven@gmail.com>
2019-10-16 11:34:16 +0000

    Testing github action

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-        title: `Thema Kunst Website`,
+        title: `Thema Kunst`,
         description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
         author: `@gatsbyjs`,
     },
