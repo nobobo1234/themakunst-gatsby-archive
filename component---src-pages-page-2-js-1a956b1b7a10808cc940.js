fafd1d904404da12927c9fcfc6dcb75c1548db92
nobobo1234 <nobobo1234@users.noreply.github.com>
2019-12-04 14:16:05 +0000

    deployed via Gatsby Publish Action 🎩 for 6afcf5d2875ab4e7229777f2211f2b6568600d48

---
+++ component---src-pages-page-2-js-1a956b1b7a10808cc940.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{224:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(145),c=a(222),r=a(221);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-1a956b1b7a10808cc940.js.map
---
+++ styles-5f765c413ec43b012a5c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{170:function(n,w,o){}}]);
+//# sourceMappingURL=styles-5f765c413ec43b012a5c.js.map
